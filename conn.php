e7e372e371f0272b1af7be5c237caab50683aa76
kiritosate <77735293+kiritosate@users.noreply.github.com>
2022-10-03 00:30:16 +0800

    Update conn.php

--- conn.php
+++ conn.php
@@ -3,10 +3,13 @@
     $username = "sql6473242";
     $password = "2GQBJqmYGT";
     $db_nam = "sql6473242";
-
-    $conn = new mysqli($s_name, $username, $password, $db_nam);
-
+    
+    
+    if($conn = new mysqli($s_name, $username, $password, $db_nam)){
     $sql = 'SELECT * FROM member';
 
     $data = mysqli_query($conn, $sql);
+    }
+
+    
 ?> 
